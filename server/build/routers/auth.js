d71a78de90d561179518b2f8847e6791bca0c422
Breyston Gonzalez <breystonbarton15@gmail.com>
2023-11-25 16:21:29 -0500

    build deleted

--- server/build/routers/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_1 = require("../controllers/auth");
-const schemaValidator_1 = require("../middlewares/schemaValidator");
-const auth_2 = require("../schemas/auth");
-exports.default = (0, express_1.Router)()
-    .post('/register', (0, schemaValidator_1.schemaValidation)(auth_2.registerSchema), auth_1.register)
-    .post('/login', (0, schemaValidator_1.schemaValidation)(auth_2.loginSchema), auth_1.login);
--- server/build/utils/hash.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.verified = exports.encrypt = void 0;
-const bcryptjs_1 = require("bcryptjs");
-const encrypt = async (pass) => await (0, bcryptjs_1.hash)(pass, 8);
-exports.encrypt = encrypt;
-const verified = async (pass, passHash) => await (0, bcryptjs_1.compare)(pass, passHash);
-exports.verified = verified;
